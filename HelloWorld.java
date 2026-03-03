a4768d0e97191fcdd449cc00dc6edd8acfbf3867
byte-size-cell <eklavya121429@gmail.com>
2026-03-03 13:36:45 +0530

    UC5:print OOPS banner app array

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,19 @@
-public class HelloWorld{
- public static void main(String[] args){
+public class BannerAppUC5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join(" ", "...", "...", "...", "..."),
+            String.join(" ", "...", "...", "...", "..."),
+            String.join(" ", "...", "...", "...", "..."),
+            String.join(" ", "...", "...", "...", "..."),
+            String.join(" ", "...", "...", "...", "..."),
+            String.join(" ", "...", "...", "...", "..."),
+            String.join(" ", "...", "...", "...", "...")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
