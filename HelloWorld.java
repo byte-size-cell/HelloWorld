30fb03ffd0deca858c19e7cdd21668d745ce2bc3
byte-size-cell <eklavya121429@gmail.com>
2026-03-03 15:15:25 +0530

    UC8-Banner using Map Collection

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,68 @@
-public class HelloWorld{
- public static void main(String[] args){
+import java.util.HashMap;
+import java.util.Map;
+
+public class BannerAppUC8 {
+
+    private static Map<Character, String[]> buildCharacterMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        patternMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    "
+        });
+
+        patternMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** "
+        });
+
+        return patternMap;
+    }
+
+    private static void renderBanner(String message, Map<Character, String[]> patternMap) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : message.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildCharacterMap();
+
+        renderBanner("OOPS", patternMap);
     }
 }
